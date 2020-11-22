a2f5ee8e23e7f4a5b786fe305ea7c1dae6a8851d
paydaylight <maxat235@gmail.com>
2020-11-22 20:22:28 +0600

    huynya rusa

--- lib/views/show_image.dart
+++ lib/views/show_image.dart
@@ -13,25 +13,37 @@ class _ShowImageScreenState extends State<ShowImageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Container(
+      body: Center(
+        child: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: FileImage(widget.parentImage),
               ),
             ),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-              height: 50,
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none, hintText: "Enter text"),
-                ),
-              )),
-        ));
+            child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                    height: 50,
+                    color: Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: "Enter text"),
+                      ),
+                    )))),
+      ),
+      // bottomNavigationBar: BottomAppBar(
+      //   child: Container(
+      //       height: 50,
+      //       child: Padding(
+      //         padding: EdgeInsets.all(10),
+      //         child: TextField(
+      //           decoration: InputDecoration(
+      //               border: InputBorder.none, hintText: "Enter text"),
+      //         ),
+      //       )),
+      // )
+    );
   }
 }
